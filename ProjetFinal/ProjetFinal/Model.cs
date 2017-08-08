7ad38dd76a315c22febfd78d4828e3ed1eb37e22
Obitruc <30499551+Obitruc@users.noreply.github.com>
2017-08-08 10:26:14 +0200

    Commit

--- ProjetFinal/ProjetFinal/Model.cs
+++ ProjetFinal/ProjetFinal/Model.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace ProjetFinal
 {
-    class Model
+    /// <summary>
+    /// 
+    /// </summary>
+    class Query
     {
+        public int QueryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DataExpiryDate { get; set; }
+        public DateTime DataTimeStamp { get; set; }
+    }
+
+    class Page
+    {
+        public int PageId { get; set; }
+        public int QueryId { get; set; }
+        public string URL { get; set; }
+
+    }
+
+    class Selector
+    {
+        public int SelectorId { get; set; }
+        public int PageId { get; set; }
+        public string Value { get; set; }
+
+    }
+
+    class ResultsHeader
+    {
+        public int ResultsHeaderId { get; set; }
+        public int SelectorId { get; set; }
+        public DateTime QueryExecutionDate { get; set; }
+    }
+
+    class ResultsDetail
+    {
+        public int ResultsDetailId { get; set; }
+        public int ResultsHeaderId { get; set; }
+        public string Value { get; set; }
     }
 }
