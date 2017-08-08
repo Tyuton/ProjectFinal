826cfb26f0e5607aeb436d1094cda2e5002f66db
Obitruc <30499551+Obitruc@users.noreply.github.com>
2017-08-08 09:53:34 +0200

    creation2 update

--- ProjetFinal/ProjetFinal/SqlScraping/ProjectScraping/ProjectScraping/ProjectScraping/creation2.sql
+++ ProjetFinal/ProjetFinal/SqlScraping/ProjectScraping/ProjectScraping/ProjectScraping/creation2.sql
@@ -67,30 +67,29 @@ ALTER TABLE [dbo].[Selector] CHECK CONSTRAINT [FK_Selector_Page]
 GO
 
 CREATE TABLE [dbo].[ResultsHeader](
-	[ResultHeaderId] [uniqueidentifier] NOT NULL,
+	[ResultsHeaderId] [uniqueidentifier] NOT NULL,
 	--[QuieryId] [uniqueidentifier] NOT NULL,
 	[SelectorId] [uniqueidentifier] NOT NULL,
 	[QuieryExecutionDate] [date] NOT NULL,
 		
  CONSTRAINT [PK_ResultsHeader] PRIMARY KEY CLUSTERED 
 (
-	[ResultHeaderId] ASC
+	[ResultsHeaderId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY] --TEXTIMAGE_ON [PRIMARY]
 
 GO
 
-ALTER TABLE [dbo].[ResultsHeader]  WITH CHECK ADD  CONSTRAINT [FK_Results_Selector] FOREIGN KEY([SelectorId])
+ALTER TABLE [dbo].[ResultsHeader]  WITH CHECK ADD  CONSTRAINT [FK_ResultsHeader_Selector] FOREIGN KEY([SelectorId])
 REFERENCES [dbo].[Selector] ([SelectorId])
 GO
 
-ALTER TABLE [dbo].[ResultsHeader] CHECK CONSTRAINT [FK_Results_Selector]
+ALTER TABLE [dbo].[ResultsHeader] CHECK CONSTRAINT [FK_ResultsHeader_Selector]
 GO
 
 CREATE TABLE [dbo].[ResultsDetails](
 	[ResultsDetailsId] [uniqueidentifier] NOT NULL,
-	[ResultsHeaderId] [uniqueidentifier] NOT NULL,
-	
+	[ResultsHeaderId] [uniqueidentifier] NOT NULL,	
 	[Value] [nvarchar](max) NOT NULL,
 		
  CONSTRAINT [PK_ResultsDetails] PRIMARY KEY CLUSTERED 
