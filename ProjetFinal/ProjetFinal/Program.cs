96401c5a2b6b856f29fc9c4839f5e49bbd836f01
Tyuton <amin.cherbal@gmail.com>
2017-08-08 16:56:14 +0200

    projet ADO .NET jour 2

--- ProjetFinal/ProjetFinal/Program.cs
+++ ProjetFinal/ProjetFinal/Program.cs
@@ -10,15 +10,32 @@ namespace ProjetFinal
     {
         static void Main(string[] args)
         {
-            Repository repo = new Repository();
-
             //Console.WriteLine("\t\tBienvenue sur l'application d'Amin et Jonathan" +
             //                  "\n\t\tVeuillez entrer une nouvelle requête");
 
-            repo.AddNewQuery();
+            Repository repo = new Repository();
+
+            repo.AddNewQuery("Ali",// query name
+                "Ali's query", //description
+                "www.ffr.fr", // url
+                "<script>", //selector script
+                new DateTime(2017,9,29), // expire les 29/9/2017
+                new DateTime(0,0,1) // répéter chaque 1 jour
+                );
+
+            var queries = repo.getQueryByName("Ali");
+
+            // Exécution : "simulation du scraping" 
             
+            repo.SetResults(queries[0],"scraping results");
+
+            var v = repo.GetResults(queries[0]);
             //Console.Read();
-            
+
+            foreach (var item in v)
+            {
+                Console.WriteLine("result. {0}", item);
+            }
               
 
         }
--- ProjetFinal/ProjetFinal/Repository.cs
+++ ProjetFinal/ProjetFinal/Repository.cs
@@ -14,13 +14,32 @@ namespace ProjetFinal
         /// TODO Add a new query...
         /// </summary>
         /// <returns></returns>
-        public bool AddNewQuery()
+        public bool AddNewQuery(string name, string des, string url, string script, DateTime expiry, DateTime timestamp)
         {
             //throw new NotImplementedException();
-            var q = new Query();
-            q.Name = "test";
-            dbQueryContext.Queries.Add(q);
+            var query = new Query();
+            query.Id = Guid.NewGuid();
+            query.Name = name;
+            query.Description = des;
+            query.DataExpiryDate = expiry;
+            query.DataTimeStamp = timestamp;
+
+            dbQueryContext.Queries.Add(query);
+
+            var page = new Page();
+            page.Id = Guid.NewGuid();
+            page.Query = query;
+            page.URL = url; 
+            dbQueryContext.Pages.Add(page);
+
+            var selector = new Selector();
+            selector.Id = Guid.NewGuid();
+            selector.Page = page;
+            selector.Value = script;
+            dbQueryContext.Selectors.Add(selector);
+
             dbQueryContext.SaveChanges();
+
             return true;
         }
         /// <summary>
@@ -28,21 +47,80 @@ namespace ProjetFinal
         /// </summary>
         /// <param name="name"></param>
         /// <returns></returns>
-        List<int> getQuery(string name)
+        public List<Query> getQueryByName(string name)
         {
-            throw new NotImplementedException();
+            return dbQueryContext.Queries.Where(q => q.Name == name).ToList();
         }
 
-        List<Object> ExecuteQuery(int queryId)
+        public bool CheckExistingQuery(Query query)
         {
-            throw new NotImplementedException();
+            return dbQueryContext.Queries.Where(q => q.Id == query.Id) != null ? true : false;
         }
 
-        bool CheckExistingQuery(int queryId)
+        //get results details
+        public List<string> GetResults(Query query)
         {
-            throw new NotImplementedException();
+            if (query == null && !CheckExistingQuery(query))
+            {
+                return null;
+            }
+            var r = (from q in dbQueryContext.Queries
+                              join p in dbQueryContext.Pages on q.Id equals p.Query.Id
+                              where q.Id == query.Id
+                              join s in dbQueryContext.Selectors on p.Id equals s.Page.Id
+                              join rh in dbQueryContext.ResultsHeaders on s.Id equals rh.Selector.Id
+                              join rd in dbQueryContext.ResultsDetails on rh.Id equals rd.ResultsHeader.Id
+                              select rd
+          )
+         .ToList();
+            List<string> sResult = new List<string>();
+            foreach (var item in r)
+            {
+                sResult.Add(item.Value);
+            }
+            return sResult;
         }
 
+        //TODO définir les params... (ResultsHeader, ResultsDetails)
+        public void SetResults(Query query, string scrapingResults)
+        {
+
+            //   var selector = dbQueryContext.Selectors
+            //.Where(s => s.Page ==
+            //   dbQueryContext.Pages
+            //   .Where(q => q.Id.ToString() == query.Id.ToString() )
+            //      ).ToList();
+
+            //   var selector = from selectors in dbQueryContext.Selectors
+            //                  join page in dbQueryContext.Pages on  equals page.QueryId into selectPage
+            //                  into dbQueryContext.Queries;
+
+            var ss = (from q in dbQueryContext.Queries
+                      join p in dbQueryContext.Pages on q.Id equals p.Query.Id
+                      where q.Id == query.Id
+                      join s in dbQueryContext.Selectors on p.Id equals s.Page.Id
+                      select s
+                      )
+                     .ToList();     
+
+            foreach (Selector item in ss)
+            {//TODO ajouter une boucle pour resultsheader
+                var resultsheader = new ResultsHeader();
+                resultsheader.Id = Guid.NewGuid();
+                resultsheader.Selector = item;
+                resultsheader.QueryExecutionDate = DateTime.Now;
+                dbQueryContext.ResultsHeaders.Add(resultsheader);
+                //TODO ajouter une boucle pour resultsdetails
+                var resultsdetails = new ResultsDetail();
+                resultsdetails.Id = Guid.NewGuid();
+                resultsdetails.ResultsHeader = resultsheader;
+                resultsdetails.Value = scrapingResults;//"scraping results";
+                dbQueryContext.ResultsDetails.Add(resultsdetails);
+            }
+
+            dbQueryContext.SaveChanges();
+
+        }
 
     }
 }
