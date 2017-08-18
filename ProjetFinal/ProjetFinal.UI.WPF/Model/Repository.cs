17cff36d46cdf7c2dee6efb58d8c6d0cce603def
Tyuton <amin.cherbal@gmail.com>
2017-08-18 10:30:48 +0200

    Fake Model

---
+++ ProjetFinal/ProjetFinal.UI.WPF/Model/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetFinal.UI.WPF.Model
+{
+    class Repository : IRepository
+    {
+        public bool AddNewQuery(string name, string des, string url, string script, DateTime expiry, DateTime timestamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckExistingQuery(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteQuery(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Query> getAllQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Query> getQueryByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetResults(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifyQuery(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetResults(Query query, string scrapingResults)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
