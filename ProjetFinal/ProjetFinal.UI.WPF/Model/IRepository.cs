4ba6eb10d69b12eb58faaeef2057b40888494115
Tyuton <amin.cherbal@gmail.com>
2017-08-18 10:28:00 +0200

    Fake Model in ProjetFinal.UI.WPF.Model

---
+++ ProjetFinal/ProjetFinal.UI.WPF/Model/IRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetFinal.UI.WPF.Model
+{
+     interface IRepository
+    {
+
+        /// <summary>
+        /// TODO Add a new query...
+        /// </summary>
+        /// <returns></returns>
+        bool AddNewQuery(string name, string des, string url, string script, DateTime expiry, DateTime timestamp);
+
+        /// <summary>
+        /// return id of queries with same "name"
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        List<Query> getQueryByName(string name);
+
+        List<Query> getAllQuery();
+
+        void ModifyQuery(Query query);
+
+        void DeleteQuery(Query query);
+
+        bool CheckExistingQuery(Query query);
+
+        //get results details
+         List<string> GetResults(Query query);
+
+         void SetResults(Query query, string scrapingResults);
+
+}
+    class Query
+    {
+
+    }
+}
