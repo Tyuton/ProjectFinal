1a6e68ef03c613aa544cbb7b3ab41c1b75e11270
Tyuton <amin.cherbal@gmail.com>
2017-09-04 15:49:48 +0200

    Requetes HTML and JS

---
+++ ProjetFinal/ProjetFinal.UI.HTML/scripts/Requetes.js
@@ -0,0 +1,39 @@
+﻿var resultsTitle = "Résultats de ("; //"Résultats de la requête selectionnée (";
+////////////////////////////////////////////
+
+$('#search').on('click', function () {
+    //TODO filter queries list by Name
+    
+})
+
+
+$('.list-group-item').on('click', function () {
+    //selected item
+    var $this = $(this);
+    $('.active').removeClass('active');
+    $this.toggleClass('active');
+
+    //update result title & list
+    updateResults($this[0]);
+
+    //update description text
+    updateDescription($this[0]);
+})
+
+function updateResults(selectedItem) {
+    //TODO update results list
+    var lresultsTitle = resultsTitle + selectedItem.text + ")";
+    $('#resultsTitle').text(lresultsTitle);
+
+    //update NbResults $('ul#menu li').length
+    var lNbResults = "Nombre d'enregistrements (" + $('ul#resultsList li').length + ")";
+    $("#NbResults").text(lNbResults);
+}
+
+function updateDescription(selectedItem) {
+    var lqueryDescription = "Description (" + selectedItem.text + ")";
+    $('#queryDescription').text(lqueryDescription);
+}
+
+
+
