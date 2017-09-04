a48fd227dfb011b250d4964549fc314975499b49
Obitruc <30499551+Obitruc@users.noreply.github.com>
2017-09-04 17:55:06 +0200

    Gerer html/css/js + FAQ.html

--- ProjetFinal/ProjetFinal.UI.HTML/scripts/Gerer.js
+++ ProjetFinal/ProjetFinal.UI.HTML/scripts/Gerer.js
@@ -1,8 +1,49 @@
-﻿$('.list-group-item').click(function () {
+﻿$('.list-group-item').on('click', function () {
     var $this = $(this);
-    $('.active').removeClass('active');
-    $this.toggleClass('active');  
+    //$this += '<span class="glyphicon glyphicon-trash">';
+
+    $('.active').removeClass('active glyphicon glyphicon-trash');
+    $this.toggleClass('active glyphicon glyphicon-trash');
+
+    showModifySelectedItem($this[0]);
 });
 
-$('.active').
+//$('.url').on('click', function () {
+//    var $this = $(this);
+//    //$this += '<span class="glyphicon glyphicon-trash">';
+
+//    $('.active').removeClass('active glyphicon glyphicon-trash');
+//    $this.toggleClass('active glyphicon glyphicon-trash');
+
+//    showModifySelectedItem($this[0]);
+//});
+
+//$('.selector').on('click', function () {
+//    var $this = $(this);
+//    //$this += '<span class="glyphicon glyphicon-trash">';
+
+//    $('.active').removeClass('active glyphicon glyphicon-trash');
+//    $this.toggleClass('active glyphicon glyphicon-trash');
+
+//    showModifySelectedItem($this[0]);
+//});
+
+
+function showModifySelectedItem(selectedItem) {
+    var item = selectedItem.text;
+    if (selectedItem.name == 'url') {
+        $('#currentURL').text(item);
+        $('.textURL').show();
+    }
+    if (selectedItem.name == 'selector') {
+        $('#currentSelector').text(item);
+        $('.textSelector').show();
+    }
+    if (selectedItem.name == 'query') {
+        $('#currentQuery').text(item);
+        $('.textQuery').show();
+    }
+    $('.hidden').hide();
+}
+
 
