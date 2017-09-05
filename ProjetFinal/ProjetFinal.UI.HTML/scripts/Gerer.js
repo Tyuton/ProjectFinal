6d4999d247e8d73c07a95f221993af0c8e9cf713
Obitruc <30499551+Obitruc@users.noreply.github.com>
2017-09-05 10:03:42 +0200

    Gerer HTML/CSS/JS

--- ProjetFinal/ProjetFinal.UI.HTML/scripts/Gerer.js
+++ ProjetFinal/ProjetFinal.UI.HTML/scripts/Gerer.js
@@ -8,42 +8,35 @@
     showModifySelectedItem($this[0]);
 });
 
-//$('.url').on('click', function () {
-//    var $this = $(this);
-//    //$this += '<span class="glyphicon glyphicon-trash">';
-
-//    $('.active').removeClass('active glyphicon glyphicon-trash');
-//    $this.toggleClass('active glyphicon glyphicon-trash');
-
-//    showModifySelectedItem($this[0]);
-//});
-
-//$('.selector').on('click', function () {
-//    var $this = $(this);
-//    //$this += '<span class="glyphicon glyphicon-trash">';
-
-//    $('.active').removeClass('active glyphicon glyphicon-trash');
-//    $this.toggleClass('active glyphicon glyphicon-trash');
-
-//    showModifySelectedItem($this[0]);
-//});
-
-
 function showModifySelectedItem(selectedItem) {
     var item = selectedItem.text;
     if (selectedItem.name == 'url') {
         $('#currentURL').text(item);
-        $('.textURL').show();
+        //$('.textURL').show();
     }
     if (selectedItem.name == 'selector') {
         $('#currentSelector').text(item);
-        $('.textSelector').show();
+        //$('.textSelector').show();
     }
     if (selectedItem.name == 'query') {
         $('#currentQuery').text(item);
-        $('.textQuery').show();
+        //$('.textQuery').show();
     }
-    $('.hidden').hide();
+    //$('.hidden').hide();
 }
 
+$('#addQuery').on('click', function () {
+    $('.textQuery').show();
+    var $this = $('#QueryList');
+    var value = $('#currentQuery').val();
+    
+    if (!(value == null || value == '')) {
+        //$this.append('<a href="#" class="list-group-item query" name="query">' + value + '</a>');
+        $this.append('<a href="#" class="list-group-item query" name="query">' + value + '</a>');
+    }
+
+    $('#currentQuery').html('');
+    
+    
+});
 
