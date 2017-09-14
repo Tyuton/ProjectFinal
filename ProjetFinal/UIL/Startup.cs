7c310035957a01698664462a7a3997213ab62ded
Obitruc <30499551+Obitruc@users.noreply.github.com>
2017-09-14 16:46:43 +0200

    Creation UIl BOL DAL WCF Projects + EF DBFirst from Database (SQLEXPRESS)

---
+++ ProjetFinal/UIL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UIL.Startup))]
+namespace UIL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
