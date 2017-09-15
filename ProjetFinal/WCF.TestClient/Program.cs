1e4f9a122aea80bd41e03325bdbd50b23fa5b2d7
Obitruc <30499551+Obitruc@users.noreply.github.com>
2017-09-15 15:08:42 +0200

    test data base access WCF.Client

--- ProjetFinal/WCF.TestClient/Program.cs
+++ ProjetFinal/WCF.TestClient/Program.cs
@@ -17,14 +17,18 @@ namespace WCF.TestClient
             //     new ChannelFactory<IService1>("Canal1");
             //IService1 serv = Canal.CreateChannel();
 
+            var repotest = new Repository();
+            var q = repotest.getQueryByName("test");
+            Console.Read();
+
 
             ChannelFactory<IRepository> Canal2 =
      new ChannelFactory<IRepository>("Canal2");
             IRepository serv2 = Canal2.CreateChannel();
 
 
-            var test = serv2.getQueryByName("test");
-            Console.WriteLine(test);
+            var test = serv2.getQueryByName("Ali");
+            Console.WriteLine(test.ToString());
             Console.WriteLine(serv2.CheckExistingQuery(test[0]));
             Console.Read();
         }
