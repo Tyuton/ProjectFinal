4221d544d4fe087046eb8b0e7986af204cd7c597
Tyuton <amin.cherbal@gmail.com>
2017-09-15 12:30:26 +0200

    make Repository public

--- ProjetFinal/WCF/Repository/Repository.cs
+++ ProjetFinal/WCF/Repository/Repository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace WCF
 {
-    class Repository : IRepository
+    public class Repository : IRepository
     {
         private WebScraperDBEntities dbContext = new WebScraperDBEntities();
 
